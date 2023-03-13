d17f34271d08d038525be1723ba085d5ac1c293a
Neto test <sashadatskaya@rambler.ru>
2023-03-13 23:08:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, Нетология!");
+        System.out.println("2 + 3");
+        System.out.println(2+3);
+
+        int myMaxSpeed = 17;
+        int x = speed +3;
+
+        double d = 6.5;
+
+        String name = "Petya";
+
+        System.out.println(name + " ехал со скоростью " + speed);
+        }
+
+
+}
